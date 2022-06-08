a1eaaab87e45353ab134198d790d858c735cf220
ipedroavelino <ipedroavelino@gmail.com>
2022-06-08 12:24:12 -0300

    unico

---
+++ script.js
@@ -0,0 +1,40 @@
+let display = document.getElementById('display')
+let minutos = document.getElementById('minutos')
+let segundos = document.getElementById('segundos')
+let comecar = document.getElementById('comecar')
+
+let cronometroSeg;
+let minutoAtual;
+let segundoAtual;
+
+let interval;
+
+for(var i =0; i<=60; i++){
+    minutos.innerHTML+='<option value="'+i+'">'+i+'</option>'
+}
+
+for(var i =1; i<=60; i++){
+    segundos.innerHTML+='<option value="'+i+'">'+i+'</option>'
+}
+
+comecar.addEventListener('click', ()=>{
+    minutoAtual = minutos.value;
+    segundoAtual = segundos.value;
+
+    display.childNodes[1].innerHTML = minutoAtual + ":" + segundoAtual;
+
+    interval = setInterval(()=>{
+        segundoAtual--;
+        if(segundoAtual <=0){
+            if(minutoAtual>0){
+                minutoAtual--;
+                segundoAtual =59;
+            }else{
+                alert("Acabou!");
+                document.getElementById("sound").play();
+                clearInterval(interval)
+            }
+        }
+        display.childNodes[1].innerHTML = minutoAtual + ":" + segundoAtual;
+    },1000)
+})
